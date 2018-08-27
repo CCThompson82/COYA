af5b2f02c7aa31b3b25afad30c40610fa171f2b4
CCThompson82 <christophercthompson82@gmail.com>
2018-08-27 12:57:57 +0100

    Refactor: clean lint

--- actonians/data/sal_website_client.py
+++ actonians/data/sal_website_client.py
@@ -11,17 +11,13 @@ MATCH_PAGE_FOR_EXTRACTION = 'http://fulltime-league.thefa.com/DisplayFixture.do?
 
 
 # TODO: Currently pulls all players for the Stewart Hyde Trophy match, which includes Poly players
-
-
 def get_registered_players():
     """
     Returns:
         pd.DataFrame of registered players
     """
     soup = make_soup(url=MATCH_PAGE_FOR_EXTRACTION)
-
     names_ls = scrape_player_names(soup=soup)
-
     registered_players = names_to_registered_dataframe(names=names_ls)
 
     return registered_players
@@ -40,7 +36,6 @@ def names_to_registered_dataframe(names):
         [], columns=['last', 'first', 'index_name'], index=range(len(names)))
 
     for idx_nb, name in enumerate(names):
-        [last, first, index_name] = parse_name(name=name)
         all_reg_players.loc[idx_nb, :] = parse_name(name=name)
 
     return all_reg_players
@@ -59,9 +54,6 @@ def parse_name(name):
         first = ' '.join(first_ls)
         name_index = '_'.join([last, first[:3]])
     return last, first, name_index
-
-
-
 
 
 def make_soup(url):
@@ -89,7 +81,7 @@ def scrape_player_names(soup):
     pattern = re.compile('DisplayStatsForPlayer')
     names_ls = []
     for tag in soup.find_all('a'):
-        if not 'href' in tag.attrs.keys():
+        if 'href' not in tag.attrs.keys():
             continue
 
         href_str = tag.attrs['href']
