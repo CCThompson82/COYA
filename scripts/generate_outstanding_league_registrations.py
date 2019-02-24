4cc8019307276e8da899af73e8f63d1b18840a1d
CCThompson82 <christophercthompson82@gmail.com>
2019-02-24 11:34:52 +0000

    Feat: Increase stringency on fuzzy matching, combined with timestamp filter

--- scripts/generate_outstanding_league_registrations.py
+++ scripts/generate_outstanding_league_registrations.py
@@ -8,7 +8,6 @@ ROOT_DIR = os.environ['ROOT_DIR']
 sys.path.append(ROOT_DIR)
 
 
-import numpy as np
 from actonians.data import gsheets_client as gs_client
 from actonians.data import sal_website_client as sal_client
 from actonians.data import db_utils
@@ -35,16 +34,22 @@ def find_outstanding_registrations():
 
     outstanding_index = db_utils.find_outstanding(
         internal=internal_registrations, external=external_registrations)
+
     raw_outstanding = raw_internal_registrations.iloc[outstanding_index].reset_index()
+    raw_ts_outstanding = db_utils.filter_by_timestamp(
+        df=raw_outstanding, key="Timestamp", months=2).reset_index()
 
     outstanding = db_utils.format_player_registrations(
-        raw_dataframe=raw_outstanding, feature_map=LEAGUE_UPLOAD_MAP)
+        raw_dataframe=raw_ts_outstanding, feature_map=LEAGUE_UPLOAD_MAP).drop_duplicates().reset_index(drop=True)
 
-    return outstanding, internal_registrations
+    return outstanding, internal_registrations, raw_ts_outstanding
 
 
 if __name__ == '__main__':
-
-    outstanding_registrations, internal_registrations = find_outstanding_registrations()
+    print('Comparing internal to external registers...')
+    outstanding_registrations, internal_registrations, ro = find_outstanding_registrations()
+    print('...completed.  {} registrations outstanding'.format(len(outstanding_registrations)))
+    print('Uploading dataframe of outstanding registrations to GS...')
     gs_client.put_outstanding_registrations(outstanding=outstanding_registrations)
-    gs_client.put_internal_registrations(internal=internal_registrations)
+    # gs_client.put_internal_registrations(internal=internal_registrations)
+    print('Process completed successfully.')
